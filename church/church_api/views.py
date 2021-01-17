069d99618d9ae8d0ddf9b95fc2cb4ff0aea27f31
Artur <aepremyan3993@gmail.com>
2021-01-17 17:03:13 +0300

    first

--- church/church_api/views.py
+++ church/church_api/views.py
@@ -29,66 +29,90 @@ def index(request):
 
 
 def asks(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/treby">Требы</a></u>'
     return render(
         request,
         'asks.html',
-        context={},
+        context={
+            "bread_crumps": bread_crumps
+        },
     )
 
 
 def shrines(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/shrines">Святыни</a></u>'
     return render(
         request,
         'shrines.html',
-        context={},
+        context={
+            "bread_crumps": bread_crumps
+        },
     )
 
 
 def formation(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/formation">Образование</a></u>'
     return render(
         request,
         "formation.html",
-        context={},
+        context={
+            "bread_crumps": bread_crumps
+        },
     )
 
 
 def ministry(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/ministry">Духовенство</a></u>'
     return render(
         request,
         "ministry.html",
-        context={}
+        context={
+            "bread_crumps": bread_crumps
+        }
     )
 
 
 def history(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/history">Храм в передельцах</a></u>'
     return render(
         request,
         "history.html",
-        context={}
+        context={
+            "bread_crumps": bread_crumps
+        }
     )
 
 
 def unction(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/sacraments">Таинства</a></u> / <u><a href="/sacraments/unction">Соборование</a></u>'
     return render(
         request,
         "unction.html",
-        context={},
+        context={
+            "bread_crumps": bread_crumps
+        },
     )
 
 
 def communion(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/sacraments">Таинства</a></u> / <u><a href="/sacraments/communion">Причастие</a></u>'
     return render(
         request,
         "communion.html",
-        context={},
+        context={
+            "bread_crumps": bread_crumps
+        },
     )
 
 
 def sacraments(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/sacraments">Таинства</a></u>'
     return render(
         request,
         "sacraments.html",
-        context={},
+        context={
+            "bread_crumps": bread_crumps
+        },
     )
 
 
@@ -102,11 +126,13 @@ def parse_shedules(request):
 
 
 def news(request):
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/news">Новости</a></u>'
     feed = News.objects.all().order_by('time').reverse()
     return render(
         request,
         'news.html',
         context={
+            "bread_crumps": bread_crumps,
             "feed": feed
         }
     )
@@ -114,10 +140,12 @@ def news(request):
 
 def schedule(request):
     schedule = PraySchedule.objects.all()
+    bread_crumps = '<u><a href="/">Главная</a></u> / <u><a href="/schedule">Расписание богослужений</a></u>'
     return render(
         request,
         'schedule.html',
         context={
+            "bread_crumps": bread_crumps,
             "schedule": schedule
         }
     )
